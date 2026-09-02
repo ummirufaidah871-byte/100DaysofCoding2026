91fe50c6fc1d666d6ab8614bdf088295ab6f13ee
Ummi <ummirufaidah871@gmail.com>
2026-09-02 22:02:26 +0800

    Create Day001.java

---
+++ Day001.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+       // Untuk memberikan komentar satu baris
+        /* Untuk memberikan komentar beberapa baris */
+        /** Untuk memberikan komentar dokumentasi */
+
+        System.out.println("Hello, World!");
+        System.out.println("Memahami struktur dasar kode Java & menggunakan semua jenis Komentar");
+    }
+}
