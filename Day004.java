4c7b1497fdd220a58e4d8591fe86461fe5b2e8b7
Ummi <ummirufaidah871@gmail.com>
2026-09-04 17:14:54 +0800

    Add Day004.java with string formatting examples

---
+++ Day004.java
@@ -0,0 +1,21 @@
+public class Day3 {
+    public static void main(String[] args) {
+        
+        // Fungsi \n
+        System.out.println("Prodi PTI\nFakultas Keguruan dan Ilmu Pendidikan\nUniversitas Sulawesi Barat");
+
+        // Fungsi \t
+        System.out.println("Nama\t: Ummi Rufaidah Hamka");
+
+        // Fungsi \b
+        System.out.println("smart\bhome");
+
+        //Fungsi \r
+        System.out.println("Saya tidak suka matcha\rkamu?");
+
+        //Fungsi double backslash(\\)
+        System.out.println("mau makan\\mau minum?");
+        
+    }
+    
+}
