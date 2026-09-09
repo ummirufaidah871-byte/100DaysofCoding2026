c6dafcee6451f14055b8db601d951380fb9b54df
Ummi <ummirufaidah871@gmail.com>
2026-09-09 15:59:19 +0800

    Create Day008.java

---
+++ Day008.java
@@ -0,0 +1,14 @@
+public class Day8 {
+public static void main(String[] args) {
+    // Deklarasi variabel 
+    String prodi;
+    
+    // inisialisasi variabel
+    prodi = "PTI";
+    System.out.println("Prodi Lama:" + prodi);
+
+    //Update variabel
+    prodi = "IPA";
+    System.out.println("Prodi Baru:"+ prodi);
+    }
+}
