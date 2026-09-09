7e546f15a67386ba86af2126cc0f078794fd3f52
Ummi <ummirufaidah871@gmail.com>
2026-09-10 05:20:54 +0800

    Add Day009.java with constant variable example

---
+++ Day009.java
@@ -0,0 +1,8 @@
+public class Day9 {
+    public static void main(String[] args) {
+        // penggunaan konstanta dalam variabel
+        final String nama = "ummi";
+        System.out.println(nama);
+        // nilai variabel dalam konstanta tidak dapat di ubah
+    }
+}
