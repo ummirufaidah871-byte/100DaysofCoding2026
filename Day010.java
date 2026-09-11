f000474c0c580e61ee20acc9433c355c63399bfe
Ummi <ummirufaidah871@gmail.com>
2026-09-11 13:53:47 +0800

    Create Day010.java

---
+++ Day010.java
@@ -0,0 +1,39 @@
+public class Day10 {
+    public static void main(String[] args) {
+        // minimal dan maksimal  tipe data byte
+        System.out.println("\nByte");
+        System.out.println("minimal:" + Byte.MIN_VALUE);
+        System.out.println("maksimal:" + Byte.MAX_VALUE);
+
+        // minimal dan maksimal tipe data short
+        System.out.println("\nShort");
+        System.out.println("minimal:" + Short.MIN_VALUE);
+        System.out.println("maksimal:" + Short.MAX_VALUE);   
+
+        // minimal dan maksimal tipe data int
+        System.out.println("\nInteger");
+        System.out.println("minimal:" + Integer.MIN_VALUE);
+        System.out.println("maksimal:" + Integer.MAX_VALUE);
+
+        //minimal dan maksimal tipe data long
+        System.out.println("\nLong");
+        System.out.println("minimal:" + Long.MIN_VALUE);
+        System.out.println("maksimal:" + Long.MAX_VALUE);
+
+        // minimal dan maksimal tipe data float
+        System.out.println("\nFloat");
+        System.out.println("minimal: " + Float.MIN_VALUE);
+        System.out.println("maksimal: " + Float.MAX_VALUE);
+
+        // minimal dan maksimal tipe data double
+        System.out.println("\nDouble");
+        System.out.println("minimal: " + Double.MIN_VALUE);
+        System.out.println("maksimal: " + Double.MAX_VALUE);
+
+        // minimal dan maksimal tipe data  char
+        System.out.println("\nChar");
+        System.out.println("minimal:"+  (int) Character.MIN_VALUE);
+         System.out.println("maksimal:"+  (int) Character.MAX_VALUE);
+    }
+    
+}
