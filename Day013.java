494868ff638e502b3c1d6fbe2bdae55460b77851
Ummi <ummirufaidah871@gmail.com>
2026-09-14 21:19:24 +0800

    Add date formatting functionality in Main class
    Implement a simple program to read three integers and format them as a date.

---
+++ Day013.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+     Scanner sc = new Scanner(System.in);
+       int kom1 = sc.nextInt();
+       int kom2 = sc.nextInt();
+       int kom3 = sc.nextInt();
+       
+       System.out.printf("0%d-%d-%d", kom1,kom2,kom3);
+    sc.close();
+    }
+}
