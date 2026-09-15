8d2d909bc92a9ad973d9749c5bc636bc0fa53670
Ummi <ummirufaidah871@gmail.com>
2026-09-15 21:30:59 +0800

    Create Day014.java

---
+++ Day014.java
@@ -0,0 +1,10 @@
+public class Day14 {
+public static void main(String[] args) {
+    
+    int x = 2;
+    int y = 4;
+
+    System.out.println("hasil 2 + 4 = "+ (x + y));
+    System.out.println("hasil 4 - 2 = "+ (y - x));
+    }
+}
